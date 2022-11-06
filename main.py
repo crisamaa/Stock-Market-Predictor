56bbfee5161a493642c07314490ee2b6d4b9b19c
crisamaa <mattia.crisanto@gmail.com>
2022-11-06 17:19:41 +0100

    data imported and plotted

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import math
 import pandas as pd
-import numpy as     
+import numpy as np
 import yfinance as yf
+import matplotlib.pyplot as plt
+import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn import metrics
+ 
+days = 10000
+stock = '^GSPC'
 
-sp500 = yf.Ticker("^GSPC")
-sp500 = sp500.history(period="max")
-print(sp500)
+
+start = (datetime.date.today() - datetime.timedelta( days ) )
+end = datetime.datetime.today()
+
+data = yf.download(stock, start=start, end=end, interval = '1d')
+print(data.head())
+
+
+plt.plot(data['Close'])
+plt.show()
